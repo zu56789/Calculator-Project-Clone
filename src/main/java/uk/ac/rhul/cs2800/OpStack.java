9384386183f02108c8f327c5995a60c428be79ef
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-02 17:08:03 +0000

    Added push and size methods so the testPush JUnit test passes

--- src/main/java/uk/ac/rhul/cs2800/OpStack.java
+++ src/main/java/uk/ac/rhul/cs2800/OpStack.java
@@ -3,4 +3,15 @@ package uk.ac.rhul.cs2800;
 public class OpStack {
   
   private Stack opStack = new Stack();
+  
+  private int size;
+  
+  public void push(Symbol i) {
+    opStack.push(new Entry(i));
+    size += 1;
+  }
+  
+  public int size() {
+    return size;
+  }
 }
--- src/test/java/uk/ac/rhul/cs2800/TestOpStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestOpStack.java
@@ -1,15 +1,24 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 class TestOpStack {
   
   private OpStack newOpStack;
 
-  @Test
-  void test() {
+  @BeforeEach
+  public void setup() {
     newOpStack = new OpStack();
+  }
+  
+  @Test
+  void testPush() {
+    newOpStack.push(Symbol.DIVIDE);
+    assertEquals(newOpStack.size(), 1);
   }
 
 }
