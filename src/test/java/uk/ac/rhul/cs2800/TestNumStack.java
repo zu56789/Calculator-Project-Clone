c1aa15a524ae080200dfcefa94f079f2ae4a66d9
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-02 03:06:05 +0000

    Finished NumStack and TestNumStack including adding checkstyledocumentation

--- src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,27 +20,27 @@ class TestNumStack {
   @Test
   void testPush() {
     newNumStack.push((float) 2.0);
-    assertEquals(newNumStack.size(), 1);
+    assertEquals(newNumStack.size(), 1, "Test push function to see if it increases the size");
   }
   
   @Test
   void testEmptyPop() {
     assertThrows(EmptyStackException.class, () -> newNumStack.pop(), 
-        "An empty stack can not be popped");
+        "An empty NumStack can not be popped");
     
   }
   
   @Test
   void testPushandPop() throws Exception {
     newNumStack.push((float) 3.0);
-    assertEquals(newNumStack.pop(), 3.0);
-    assertEquals(newNumStack.size(), 0);
+    assertEquals(newNumStack.pop(), 3.0, "If you push 3.0 then it should be popped");
+    assertEquals(newNumStack.size(), 0, "NumStack should be empty after pop");
     
   }
   
   @Test
   void testifEmpty() {
-    assertEquals(newNumStack.isEmpty(), true);
+    assertEquals(newNumStack.isEmpty(), true, "NumStack should start off empty.");
   }
 
 }
