03479a3a4d78534daf86507cf64b994e41efbda4
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-02 18:59:12 +0000

    Added javadoc and fixed checkstyle issues

--- src/test/java/uk/ac/rhul/cs2800/TestOpStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestOpStack.java
@@ -20,31 +20,35 @@ class TestOpStack {
   @Test
   void testPush() {
     newOpStack.push(Symbol.DIVIDE);
-    assertEquals(newOpStack.size(), 1);
+    assertEquals(newOpStack.size(), 1, "Tests if pushing increases the size.");
   }
   
   @Test
   void testEmptyPop() {
-    assertThrows(EmptyStackException.class, () -> newOpStack.pop());
+    assertThrows(EmptyStackException.class, () -> newOpStack.pop(),
+        "An empty OpStack that is popped should throw an EmptyStackException.");
   }
   
   @Test
   void testPushandPop() throws Exception {
     newOpStack.push(Symbol.LEFT_BRACKET);
-    assertEquals(newOpStack.pop(), Symbol.LEFT_BRACKET);
-    assertEquals(newOpStack.size(), 0);
+    assertEquals(newOpStack.pop(), Symbol.LEFT_BRACKET,
+        "if you push a symbol onto Opstack, the same one should be popped.");
+    assertEquals(newOpStack.size(), 0, "Popping should also decrease size.");
     
   }
   
   @Test
   void testEmptyTop()  {
-    assertThrows(EmptyStackException.class, () -> newOpStack.top());
+    assertThrows(EmptyStackException.class, () -> newOpStack.top(),
+        "Calling top on an empty OpStack should throw an EmptyStackException.");
   }
   
   @Test
   void testPushandTop() throws Exception {
     newOpStack.push(Symbol.MINUS);
-    assertEquals(newOpStack.top(), Symbol.MINUS);
+    assertEquals(newOpStack.top(), Symbol.MINUS,
+        "Symbol.MINUS is at the top of the OpStack so it should be returned by top.");
     
   }
   
@@ -52,20 +56,24 @@ class TestOpStack {
   void testPushPopandTop() throws Exception {
     newOpStack.push(Symbol.INVALID);
     newOpStack.pop();
-    assertThrows(EmptyStackException.class, () -> newOpStack.top());
+    assertThrows(EmptyStackException.class, () -> newOpStack.top(),
+        "Calling top after pushing and popping should throw an EmptyStackException.");
   }
   
   @Test
   void testisEmpty() {
-    assertEquals(newOpStack.isEmpty(), true);
+    assertEquals(newOpStack.isEmpty(), true, 
+        "Testing an Empty OpStack returns true when isEmpty is called.");
   }
   
   @Test
   void testPushandisEmpty() throws Exception {
     newOpStack.push(Symbol.TIMES);
-    assertEquals(newOpStack.isEmpty(), false);
+    assertEquals(newOpStack.isEmpty(), false, 
+        "Testing a non empty stack returns false when isEmpty is called.");
     newOpStack.pop();
-    assertEquals(newOpStack.isEmpty(), true);
+    assertEquals(newOpStack.isEmpty(), true, 
+        "After popping the value, the OpStack should be empty.");
   }
   
   
