544ec63dbe89fa020b4f781de70a676cc7c0d6b6
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-03 21:07:21 +0000

    Added push and size methods

--- src/main/java/uk/ac/rhul/cs2800/StrStack.java
+++ src/main/java/uk/ac/rhul/cs2800/StrStack.java
@@ -1,6 +1,17 @@
 package uk.ac.rhul.cs2800;
 
 public class StrStack {
+  
   private Stack strStack = new Stack();
+  private int size;
+  
+  public void push(String i) {
+    strStack.push(new Entry(i));
+    size += 1;
+  }
+  
+  public int size() {
+    return size;
+  }
   
 }
--- src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
@@ -1,14 +1,23 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestStrStack {
   
+  private StrStack newStrStack;
 
+  @BeforeEach
+  public void setup() {
+    newStrStack = new StrStack(); 
+  }
+  
   @Test
-  void test() {
-    StrStack newStrStack = new StrStack(); 
+  void testPush() {
+    newStrStack.push("Hello");
+    assertEquals(newStrStack.size(), 1);
   }
 
 }
