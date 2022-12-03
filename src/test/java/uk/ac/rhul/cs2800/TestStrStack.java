462084d674d925a36789f11a5af45e76962313c1
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-03 21:54:06 +0000

    added javadoc and removed checkstyle issues

--- src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,30 +19,38 @@ class TestStrStack {
   @Test
   void testPush() {
     newStrStack.push("Hello");
-    assertEquals(newStrStack.size(), 1);
+    assertEquals(newStrStack.size(), 1, "Tests if pushing increases the size.");
   }
   
   @Test
   void testEmptyPop() {
-    assertThrows(EmptyStackException.class, () -> newStrStack.pop());
+    assertThrows(EmptyStackException.class, () -> newStrStack.pop(),
+        "An empty StrStack that is popped should throw an EmptyStackException.");
   }
   
   @Test
   void testPushandPop() throws Exception {
     newStrStack.push("Hello mate");
-    assertEquals(newStrStack.pop(), "Hello mate");
-    assertEquals(newStrStack.size(), 0);
+    assertEquals(newStrStack.pop(), "Hello mate",
+        "if you push a String onto StrStack, the same one should be popped.");
+    assertEquals(newStrStack.size(), 0, "popping should decrease size by 1.");
   }
   
   @Test
   void testisEmpty() {
-    assertEquals(newStrStack.isEmpty(), true);
+    assertEquals(newStrStack.isEmpty(), true,
+        "Testing an Empty StrStack returns true when isEmpty is called.");
   }
   
   @Test
-  void testPushandisEmpty() {
+  void testPushandisEmpty() throws Exception {
     newStrStack.push("Hello");
-    assertEquals(newStrStack.isEmpty(), false);
+    assertEquals(newStrStack.isEmpty(), false,
+        "Testing a non empty StrStack returns false when isEmpty is called.");
+    newStrStack.pop();
+    assertEquals(newStrStack.isEmpty(), true,
+        "After popping the value, the StrStack should be empty.");
+    
   }
 
 }
