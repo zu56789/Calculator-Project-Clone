af502d06ec8cd8ee3cc03981fa1139aff9660f4f
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-04 02:22:39 +0000

    Added fake return value to pass test1

--- src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -1,16 +1,22 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestRevPolishCalc {
 
   private RevPolishCalc rpnCalc;
   
-  @Test
-  void test() {
+  @BeforeEach
+  public void test() {
     rpnCalc = new RevPolishCalc();
   }
   
+  @Test
+  void testValidEvaluate() throws Exception {
+    assertEquals(rpnCalc.evaluate(""), 0);
+  }
 
 }
