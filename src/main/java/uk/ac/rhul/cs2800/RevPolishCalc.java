46a5ae7fbd694a48a1650302bddda9bca776600f
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-04 02:13:45 +0000

    created RevPolishCalc class

---
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -0,0 +1,12 @@
+package uk.ac.rhul.cs2800;
+
+public class RevPolishCalc implements Calculator {
+
+  private NumStack values = new NumStack();
+  
+  @Override
+  public float evaluate(String expression) throws Exception {
+    return 0;
+  }
+
+}
---
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -0,0 +1,16 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestRevPolishCalc {
+
+  private RevPolishCalc rpnCalc;
+  
+  @Test
+  void test() {
+    rpnCalc = new RevPolishCalc();
+  }
+  
+
+}
