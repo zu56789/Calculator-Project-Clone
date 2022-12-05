872fb7fd1782f6cfc44f4fcafe9b8c78df0908b3
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-05 05:42:28 +0000

    Fully functioning RevPolishCalc with javadoc and checkstyle

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -3,30 +3,42 @@ package uk.ac.rhul.cs2800;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * this class evaluates RPN string expressions.
+
+ * @author Zuhayr
+ *
+ */
 public class RevPolishCalc implements Calculator {
 
   private NumStack values = new NumStack();
   private HashMap<Symbol, String> operatorMap = new HashMap<Symbol, String>();
+  //Hashmap is used so I can implement the Symbol enum and assign each a value.
 
+  /**
+   * this method calculates the solution to the rpn expression passed in to it.
+   */
   @Override
   public float evaluate(String expression) throws Exception {
     operatorMap.put(Symbol.PLUS, "+");
     operatorMap.put(Symbol.MINUS, "-");
     operatorMap.put(Symbol.TIMES, "*");
     operatorMap.put(Symbol.DIVIDE, "/");
-    Scanner scan = new Scanner(expression);
+    Scanner scan = new Scanner(expression); // reads contents of expression. 
     while (scan.hasNext()) {
       String value = scan.next();
-      if (operatorMap.containsValue(value) || value.matches("^[0-9]*$")) {
-        
+      //check if the next string value is an operator or a number.
+      if (operatorMap.containsValue(value) || value.matches("^[0-9]*$")) { 
+        //when value is a number, push it to the stack and continue the while loop.
         if (!operatorMap.containsValue(value)) {
           values.push(Float.valueOf(value));
           continue;
         }
+        // when value is an operator but there arent 2 numbers in the stack, throw exception.
         if (values.size() < 2) {
           throw new IllegalArgumentException("Invalid Expression");
         }
-      } else {
+      } else { // anything other than an operator/number is invalid.
         throw new IllegalArgumentException("Invalid Expression"); 
       }
       
@@ -40,7 +52,7 @@ public class RevPolishCalc implements Calculator {
       } else if (value.equals(operatorMap.get(Symbol.TIMES))) {
         values.push(a * b);
       } else if (value.equals(operatorMap.get(Symbol.DIVIDE))) {
-        
+        //do not want division by zero to generate infinity.
         if (a == 0 || b == 0) {
           throw new ArithmeticException("Cannot Divide by 0");
         }
--- src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,52 +17,62 @@ class TestRevPolishCalc {
   
   @Test
   void testInvalidExpression1() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("4+32+"));
+    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("4+32+"), 
+        "Tests that inputting 1 string throws an exception");
   }
   
   @Test
   void testInvalidExpression2() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("2f 4 +"));
+    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("2f 4 +"), 
+        "Tests that an expression with a letter throws an exception");
   }
   
   @Test
   void testInvalidExpression3() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("6 5 ;"));
+    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("6 5 ;"), 
+        "Tests that an expression without a random symbol throws an exception");
   }
   
   @Test
   void testInvalidExpression4() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("5 +"));
+    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("5 +"), 
+        "Tests that an expression with not enough operands throws an exception");
   }
   
   @Test
   void testInvalidExpression5() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("5 5 + +"));
+    assertThrows(IllegalArgumentException.class, () -> rpnCalc.evaluate("5 5 + +"), 
+        "Tests that an expression with too many operators throws an exception");
   }
   
   @Test
   void testAdd() throws Exception {
-    assertEquals(rpnCalc.evaluate("5 5 +"), 10);
+    assertEquals(rpnCalc.evaluate("5 5 +"), 10, 
+        "Tests that an addition expression is correctly evaluated");
   }
   
   @Test
   void testMinus() throws Exception {
-    assertEquals(rpnCalc.evaluate("5 5 -"), 0);
+    assertEquals(rpnCalc.evaluate("5 5 -"), 0, 
+        "Tests that a subtraction expression is correctly evaluated");
   }
   
   @Test
   void testMultiply() throws Exception {
-    assertEquals(rpnCalc.evaluate("5 5 *"), 25);
+    assertEquals(rpnCalc.evaluate("5 5 *"), 25,
+        "Tests that a multiplication expression is correctly evaluated");
   }
   
   @Test
   void testDivide() throws Exception {
-    assertEquals(rpnCalc.evaluate("10 5 /"), 2);
+    assertEquals(rpnCalc.evaluate("10 5 /"), 2, 
+        "Tests that a division expression is correctly evaluated");
   }
   
   @Test
   void testDivideZero() throws Exception {
-    assertThrows(ArithmeticException.class, () -> rpnCalc.evaluate("0 9 /"));
+    assertThrows(ArithmeticException.class, () -> rpnCalc.evaluate("0 9 /"),
+        "Tests that diving by zero throws an exception");
   }
   
 }
