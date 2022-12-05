2c5ec01dc3b30cf805a51dcd055805ad499803c5
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-05 19:30:04 +0000

    Created StandardCalc

---
+++ src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -0,0 +1,11 @@
+package uk.ac.rhul.cs2800;
+
+public class StandardCalc implements Calculator {
+
+  @Override
+  public float evaluate(String expression) throws Exception {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
