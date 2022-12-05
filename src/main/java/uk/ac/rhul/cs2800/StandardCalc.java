807dd0721da90cb37dc8386fc84b0e141b47d574
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-05 19:41:57 +0000

    Created infixtoRpn method

--- src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -2,6 +2,14 @@ package uk.ac.rhul.cs2800;
 
 public class StandardCalc implements Calculator {
 
+  private OpStack values = new OpStack();
+  private RevPolishCalc rpnCalc;
+  
+  
+  public String infixtoRpn(String expression) {
+    return "";
+  }
+  
   @Override
   public float evaluate(String expression) throws Exception {
     // TODO Auto-generated method stub
--- src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
@@ -1,15 +1,22 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 class TestStandardCalc {
 
   private StandardCalc newStandardCalc;
   
-  @Test
-  void test() {
+  @BeforeEach
+  public void test() {
     newStandardCalc = new StandardCalc();
+  }
+  
+  @Test
+  void testinfixtoRpn() {
+    assertEquals(newStandardCalc.infixtoRpn(""), "");
   }
 
 }
