bb2a31b56a2eb5d4e7969a2b7fd095644308b1c7
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-06 05:22:05 +0000

    Added Evaluate method with fake test value

--- src/main/java/uk/ac/rhul/cs2800/CalcModel.java
+++ src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -2,4 +2,10 @@ package uk.ac.rhul.cs2800;
 
 public class CalcModel {
   
+  private RevPolishCalc revPolishCalc = new RevPolishCalc();
+  private StandardCalc standardCalc = new StandardCalc();
+  
+  public float evaluate(String expression, boolean infix) {
+    return 0;
+  }
 }
--- src/test/java/uk/ac/rhul/cs2800/TestCalcModel.java
+++ src/test/java/uk/ac/rhul/cs2800/TestCalcModel.java
@@ -1,15 +1,22 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCalcModel {
 
   private CalcModel calcModel;
   
-  @Test
-  void test() {
+  @BeforeEach
+  public void test() {
     calcModel = new CalcModel();
+  }
+  
+  @Test
+  void testEvaluate() {
+    assertEquals(calcModel.evaluate("", false), 0);
   }
 
 }
