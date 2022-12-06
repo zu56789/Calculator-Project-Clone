9de4357023e13c998428ee178d00694e77f02e39
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-06 00:55:56 +0000

    StandardCalc is fully functioning and also has been fully documented

--- src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestStandardCalc.java
@@ -1,6 +1,7 @@
 package uk.ac.rhul.cs2800;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,66 +16,65 @@ class TestStandardCalc {
   }
   
   @Test
-  void testinfixtoRpn() {
-    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn(""));
-  }
-  
-  @Test
-  void testInvalidExpression1() {
-    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5+5"));
+  void testInvalidExpression() {
+    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn(""), 
+        "Tests that an empty expression throws an exception.");
   }
   
   @Test
   void testInvalidExpression2() {
-    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5fg5"));
+    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5+5"), 
+        "Tests that a single string expression throws an exception.");
   }
   
   @Test
   void testInvalidExpression3() {
-    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5 + 5;"));
+    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5fg5"), 
+        "Tests that any non bracket, non number or non operator throws an expression");
   }
+  
   
   @Test
   void testInvalidExpression4() {
-    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5"));
-  }
-  
-  @Test
-  void testoutput() throws Exception {
-    assertEquals(newStandardCalc.infixtoRpn("5 5 5"), "5 5 5");
-  }
-  
-  @Test
-  void testoutput2() throws Exception {
-    assertEquals(newStandardCalc.infixtoRpn("( 5  5 )"), "5 5");
+    assertThrows(IllegalArgumentException.class, () -> newStandardCalc.infixtoRpn("5"),
+        "Tests that an incomplete expression throws an exception");
   }
   
   @Test
   void testgetPrecedenceAddorSubtract() {
-    assertEquals(newStandardCalc.getPrecedence("+"), 1);
-    assertEquals(newStandardCalc.getPrecedence("-"), 1);
+    assertEquals(newStandardCalc.getPrecedence("+"), 1, 
+        "Tests that the plus operator has a precedence of 1.");
+    assertEquals(newStandardCalc.getPrecedence("-"), 1,
+        "Tests that the minus operator has a precedence of 1.");
   }
   
   @Test
   void testgetPrecedenceTimesorDivide() {
-    assertEquals(newStandardCalc.getPrecedence("*"), 2);
-    assertEquals(newStandardCalc.getPrecedence("/"), 2);
+    assertEquals(newStandardCalc.getPrecedence("*"), 2,
+        "Tests that the multiply operator has a precedence of 2.");
+    assertEquals(newStandardCalc.getPrecedence("/"), 2, 
+        "Tests that the divide operator has a precedence of 2.");
   }
   
   @Test
   void testoutput3() throws Exception {
-    assertEquals(newStandardCalc.infixtoRpn("( 5 * ( 6 + 7 ) ) - 2"), "5 6 7 + * 2 -");
+    assertEquals(newStandardCalc.infixtoRpn("5 + 5"), "5 5 +",
+        "Tests that an infix expression is successfully converted to postfix.");
   }
   
   @Test
   void testoutput4() throws Exception {
-    assertEquals(newStandardCalc.infixtoRpn("( 5 + 6 ) + ( 7 * 4 )"), "5 6 + 7 4 * +");
+    assertEquals(newStandardCalc.infixtoRpn("( 5 + 6 ) + ( 7 * 4 )"), "5 6 + 7 4 * +",
+        "Tests a more complex infix expression is successfully converted to postfix.");
   }
   
   @Test
   void testevaluate() throws Exception {
     String expression = newStandardCalc.infixtoRpn("( 5 + 6 ) + ( 7 * 4 )");
-    assertEquals(newStandardCalc.evaluate(expression), 39);
+    assertEquals(newStandardCalc.evaluate(expression), 39,
+        "Tests that an infix expression is successfully evaluated.");
   }
+  
+ 
 
 }
