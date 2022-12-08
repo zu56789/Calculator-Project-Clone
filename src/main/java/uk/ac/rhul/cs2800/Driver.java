369f6fe16be5956b5168545ad6f3c18ddd8bae83
Zuhayr M <zkac412@live.rhul.ac.uk>
2022-12-08 02:11:37 +0000

    GUI successfully implemented. Calculator now has full functionality.

--- src/main/java/uk/ac/rhul/cs2800/Driver.java
+++ src/main/java/uk/ac/rhul/cs2800/Driver.java
@@ -20,7 +20,6 @@ public class Driver extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    
     Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MyView.fxml"));
     Scene scene = new Scene(root, 700, 400);
     primaryStage.setScene(scene);
--- src/main/java/uk/ac/rhul/cs2800/MyView.java
+++ src/main/java/uk/ac/rhul/cs2800/MyView.java
@@ -1,11 +1,15 @@
 package uk.ac.rhul.cs2800;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 
 /**
@@ -15,7 +19,10 @@ import javafx.scene.layout.Pane;
  *
  */
 public class MyView {
-
+  
+  private CalcModel model = new CalcModel();
+  private boolean infix;
+  
   @FXML
   private Button calculateButton;
 
@@ -39,10 +46,42 @@ public class MyView {
 
   @FXML
   private RadioButton rpnRadioButton;
-    
+  
   @FXML
-  void isPressed(ActionEvent event) {
-
+  private ToggleGroup rpnorinfix;
+   
+  @FXML
+  void isPressed(ActionEvent event)  {
+    expressionType();
+    try {
+      resultField.setText(String.valueOf(model.evaluate(expressionField.getText(), infix)));
+    } catch (Exception e) {
+      resultField.setText("Invalid Expression");
+    } 
   }
 
+  /**
+   * this method uses a radioButton ToggleGroup listener 
+   * to determine the correct value of the boolean infix value.
+   */
+  public void expressionType() {
+    rpnorinfix.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+      @Override
+      public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue,
+          Toggle newValue) {
+        RadioButton rb = (RadioButton) rpnorinfix.getSelectedToggle();
+        
+        if (rb != null) {
+          String s = rb.getText();
+          if (s.equals("Infix")) {
+            infix = true;
+          } else {
+            infix = false;
+          }
+        }
+      }
+    });
+  }
+  
 }
